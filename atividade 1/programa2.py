9da4d067540e0aa65b7ef996797fd30c9ba178f7
davicruz-moreira-dot <davi_cruz-moreira@estudante.sesisenai.org.br>
2026-02-27 19:44:13 +0000

    update

---
+++ atividade 1/programa2.py
@@ -0,0 +1,4 @@
+nome = input("insira seu nome")
+numero1 = input("insira o primeiro numero")
+numero2 = input("insira o seugndo numero")
+print("o usuario" , nome, "obiteve a soma sod valores de ",numero1+numero2)
---
+++ atividade 1/programa3.py
@@ -0,0 +1,4 @@
+nome = input("insira seu nome")
+numero1 = int(input("insira o primeiro numero"))
+numero2 = int(input("insira o seugndo numero"))
+print("o usuario" , nome, "obiteve a soma dos valores de ",numero1+numero2)
